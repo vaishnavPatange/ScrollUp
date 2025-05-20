843866ac9e777adc0537a5c73510609a2c595a1e
vaishnavPatange <patangevaishnav5@gmail.com>
2025-05-20 16:41:00 +0530

    add db connection

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,38 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = process.env.MONGODB_URL;
+const MONGO_URL = process.env.MONGODB_URL!;
+
+if(!MONGO_URL){
+  throw new Error("please add mongodb url in .env file");
+}
 
 let cached = global.mongoose;
+
+if(!cached){
+  cached = global.mongoose = {conn: null, promise: null}
+}
+
+export async function connectDB(){
+
+  if(cached.conn) return cached.conn;
+
+  if(!cached.promise){
+    const opts = {
+      bufferCommands: true,
+      maxPoolSize: 10
+    }
+
+    cached.promise = mongoose.connect(MONGO_URL, opts)
+                      .then(() => mongoose.connection)
+  }
+
+  try {
+    cached.conn = await cached.promise
+  } catch (error) {
+    cached.promise = null
+    throw error
+  }
+
+  return cached.conn
+
+}
